df6e2e65925274b4370fb90141c5a98c99733443
Paul Dix <paul@pauldix.net>
2009-12-28 16:39:37 -0500

    fixed spec helper to point to extractule

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,4 @@ begin require "redgreen" unless ENV['TM_CURRENT_LINE']; rescue LoadError; end
 path = File.expand_path(File.dirname(__FILE__) + "/../lib/")
 $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
 
-require "lib/domainatrix"
+require "lib/extractula"
