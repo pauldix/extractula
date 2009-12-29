1eb40c2bb0eebb67a64b6111fac4b865aa90b3b6
Paul Dix <paul@pauldix.net>
2009-12-29 12:38:00 -0500

    added the public interface for the Extractula module

---
+++ lib/extractula.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
+
+module Extractula; end
+
+require 'nokogiri'
+require 'extractula/extracted_content'
+require 'extractula/dom_extractor'
+
+module Extractula
+  @extractors = []
+
+  def self.add_extractor(extractor_class)
+    @extractors << extractor_class
+  end
+
+  def self.remove_extractor(extractor_class)
+    @extractors.delete extractor_class
+  end
+
+  def self.extract(url, html)
+    extractor = @extractors.detect {|e| e.can_extract? url, html} || DomExtractor
+    extractor.new.extract(url, html)
+  end
+end
---
+++ spec/extractula_spec.rb
@@ -0,0 +1,45 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "extractula" do
+  it "can add custom extractors" do
+    custom_extractor = Class.new do
+      def self.can_extract? url, html
+        true
+      end
+
+      def extract url, html
+        Extractula::ExtractedContent.new :url => "custom extractor url", :summary => "my custom extractor"
+      end
+    end
+
+    Extractula.add_extractor custom_extractor
+    content = Extractula.extract("http://pauldix.net", "some html")
+    content.url.should == "custom extractor url"
+    content.summary.should == "my custom extractor"
+    Extractula.remove_extractor custom_extractor
+  end
+
+  it "skips custom extractors that can't extract the passed url and html" do
+    custom_extractor = Class.new do
+      def self.can_extract? url, html
+        false
+      end
+
+      def extract url, html
+        Extractula::ExtractedContent.new :url => "this url", :summary => "this summary"
+      end
+    end
+
+    Extractula.add_extractor custom_extractor
+    content = Extractula.extract("http://pauldix.net", "some html")
+    content.url.should_not == "this url"
+    content.summary.should_not == "this summary"
+    Extractula.remove_extractor custom_extractor
+  end
+
+  it "extracts from a url and document and returns an ExtractedContent object" do
+    result = Extractula.extract("http://pauldix.net", "")
+    result.should be_a Extractula::ExtractedContent
+    result.url.should == "http://pauldix.net"
+  end
+end
