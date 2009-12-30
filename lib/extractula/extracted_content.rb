09f0e1be5f1ba7dd226b4d6a86342b48f3d8c20b
Paul Dix <paul@pauldix.net>
2009-12-30 14:45:12 -0500

    added summary and image extraction

--- lib/extractula/extracted_content.rb
+++ lib/extractula/extracted_content.rb
@@ -1,7 +1,26 @@
 class Extractula::ExtractedContent
-  attr_reader :url, :title, :summary, :content, :image_urls, :video_embed
+  attr_reader :url, :title, :content, :video_embed
 
   def initialize(attributes = {})
     attributes.each_pair {|k, v| instance_variable_set("@#{k}", v)}
+  end
+
+  def summary
+    return @summary if @summary
+    @content_doc ||= Nokogiri::HTML(@content)
+    content_fragment = @content_doc.inner_text.slice(0, 350)
+    sentence_break = content_fragment.rindex(/\?|\.|\!|\;/)
+    if sentence_break
+      @summary = content_fragment.slice(0, sentence_break + 1)
+      @summary
+    else
+      @summary = content_fragment
+    end
+  end
+
+  def image_urls
+    return @image_urls if @image_urls
+    @content_doc ||= Nokogiri::HTML(@content)
+    @image_urls = @content_doc.search("//img").collect {|t| t["src"]}
   end
 end
--- spec/extractula/extracted_content_spec.rb
+++ spec/extractula/extracted_content_spec.rb
@@ -9,12 +9,30 @@ describe "extracted content" do
     Extractula::ExtractedContent.new(:title => "whatevs").title.should == "whatevs"
   end
 
-  it "has a summary" do
-    Extractula::ExtractedContent.new(:summary => "a summary!").summary.should == "a summary!"
+  it "has content" do
+    Extractula::ExtractedContent.new(:content => "some content").content.should == "some content"
   end
 
-  it "has image_urls" do
-    Extractula::ExtractedContent.new(:image_urls => ["first.jpg", "second.tiff"]).image_urls.should == ["first.jpg", "second.tiff"]
+  describe "summary" do
+    it "has a summary" do
+      Extractula::ExtractedContent.new(:summary => "a summary!").summary.should == "a summary!"
+    end
+
+    it "generates the summary from the content" do
+      extracted = Extractula::ExtractedContent.new(:content => "<p>I've been quietly working on Typhoeus for the last few months. With the help of <a href=\"http://metaclass.org/\">Wilson Bilkovich</a> and <a href=\"http://github.com/dbalatero\">David Balatero</a> I've finished what I think is a significant improvement to the library. The new interface removes all the magic and opts instead for clarity.</p>\n<p>It's really slick and includes improved stubing support, caching, memoization, and (of course) parallelism. The <a href=\"http://github.com/pauldix/typhoeus/\">Typhoeus readme</a> highlights all of the awesomeness. It should be noted that the old interface of including Typhoeus into classes and defining remote methods has been deprecated. I'll be removing that sometime in the next six months.</p>\n<p>In addition to thanking everyone using the library and everyone contributing, I should also thank my employer kgbweb. If you're a solid Rubyist that likes parsing, crawling, and stuff, or a machine learning guy, or a Solr/Lucene/indexing bad ass, let me know. We need you and we're doing some crazy awesome stuff.</p>")
+      extracted.summary.should == "I've been quietly working on Typhoeus for the last few months. With the help of Wilson Bilkovich and David Balatero I've finished what I think is a significant improvement to the library. The new interface removes all the magic and opts instead for clarity."
+    end
+  end
+
+  describe "image_urls" do
+    it "has image_urls" do
+      Extractula::ExtractedContent.new(:image_urls => ["first.jpg", "second.tiff"]).image_urls.should == ["first.jpg", "second.tiff"]
+    end
+
+    it "generates the image urls from the content" do
+      extracted = Extractula::ExtractedContent.new(:content => "<p><a href=\"http://www.businessinsider.com/10-stunning-web-site-prototype-sketches-2009-12/early-ember-1\"><img class=\"float_right\" src=\"http://static.businessinsider.com/~~/f?id=4b3a466f000000000086e662&amp;maxX=311&amp;maxY=233\" border=\"0\" height=\"233\" alt=\"Web site wireframes\" width=\"311\"></a></p>\n<div style=\"float: left; padding: 15px 15px 15px 0;\">\n\n</div>\n<p>When designers start a new Web site, they often sketch out a first idea of the page layout using paper and stencil.&nbsp;</p>\n<p>Designers call this sketch a \"wireframe.\"</p>\n<p>Woorkup.com's Antonio Lupetti <a href=\"http://woorkup.com/2009/12/28/10-beautiful-sketches-for-website-prototypes/\">collected</a> 10 beautiful examples of wireframes.</p>\n<p><a href=\"http://www.businessinsider.com/10-stunning-web-site-prototype-sketches-2009-12/early-ember-1\"><strong>He gave us permission to republish them here &gt;</strong></a></p>")
+      extracted.image_urls.should == ["http://static.businessinsider.com/~~/f?id=4b3a466f000000000086e662&maxX=311&maxY=233"]
+    end
   end
 
   it "has a video_embed" do
