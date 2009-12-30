99b50159e7b0dc7942d73c886e833f4f753fc3d7
Sander Hartlage <sander@kgbweb.com>
2009-12-30 17:24:22 -0500

    Requiring custom extractors into the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ path = File.expand_path(File.dirname(__FILE__) + "/../lib/")
 $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
 
 require "lib/extractula"
+require "lib/extractula/custom_extractors"
 
 def read_test_file(file_name)
   File.read("#{File.dirname(__FILE__)}/test-files/#{file_name}")
