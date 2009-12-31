ba2986acd722e197756b0a111288654b009c017c
Sander Hartlage <sander.hartlage@gmail.com>
2009-12-31 11:33:53 -0500

    Beefed up extractor spec (a little bit)

--- spec/extractula/extractor_spec.rb
+++ spec/extractula/extractor_spec.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Extractula::Extractor do
   
   before do
-    @url  = "http://www.website.com/"
-    @html = ""
+    @url  = Domainatrix.parse("http://www.website.com/")
+    @html = Nokogiri::HTML::Document.new
   end
   
   it "should not be able to extract anything" do
@@ -13,7 +13,7 @@ describe Extractula::Extractor do
   
   describe "extracting" do
     it "should give an empty ExtractedContent object" do
-      content = Extractula::Extractor.new.extract(@url, @html)
+      content = Extractula::Extractor.new(@url, @html).extract
       content.title.should be_nil
       content.summary.should be_empty
       content.image_urls.should be_empty
@@ -22,9 +22,22 @@ describe Extractula::Extractor do
   end
   
   describe "when subclassing" do
-    it "should add the subclass as an extractor to the Extractula module" do
+    before do
       class Thingy < Extractula::Extractor; end
+    end
+
+    it "should add the subclass as an extractor to the Extractula module" do
       Extractula.instance_variable_get(:@extractors).should include(Thingy)
     end
+    
+    describe "setting the domain" do
+      before do
+        Thingy.domain 'website'
+      end
+      
+      it "should be able to extract urls from that domain" do
+        Thingy.can_extract?(@url, @html).should be_true
+      end
+    end    
   end
 end
