d42b9b9efceb732f1cf81a95e64a9da5a0ee044f
Sander Hartlage <sander@kgbweb.com>
2010-01-04 17:48:42 -0500

    Added crazy defaults hash and directive to use those defaults to the extractor base class

--- lib/extractula/extractor.rb
+++ lib/extractula/extractor.rb
@@ -5,6 +5,65 @@
 module Extractula
   class Extractor
     
+    DEFAULTS = {
+      :title        => 'title',
+      :summary      => Proc.new { excerpt(content) },
+      :image_urls   => 'img',
+      :video_embed  => 'object',
+      :content      => Proc.new {
+        def calculate_children_text_size(parent, node_type)
+          text_size = 0
+          parent.children.each do |child|
+            if child.node_name == node_type
+              child.children.each {|c| text_size += c.text.strip.size if c.node_name == "text"}
+            end
+          end
+
+          text_size
+        end
+
+        candidate_nodes = html.search("//div|//p|//br").collect do |node|
+          parent = node.parent
+          if node.node_name == 'div'
+            text_size = calculate_children_text_size(parent, "div")
+
+            if text_size > 0
+              {:text_size => text_size, :parent => parent}
+            else
+              nil
+            end
+          elsif node.node_name == "p"
+            text_size = calculate_children_text_size(parent, "p")
+
+            if text_size > 0
+              {:text_size => text_size, :parent => parent}
+            else
+              nil
+            end
+          elsif node.node_name == "br"
+            if node.previous.node_name == "text" && node.next.node_name == "text"
+              text_size = 0
+              parent.children.each do |child|
+                text_size += child.text.strip.size if child.node_name == "text"
+              end
+
+              if text_size > 0
+                {:text_size => text_size, :parent => parent}
+              else
+                nil
+              end
+            else
+              nil
+            end
+          else
+            nil
+          end
+        end.compact.uniq
+
+        fragment = candidate_nodes.detect {|n| n[:text_size] > 140}[:parent].inner_html.strip rescue ""
+      }
+    }
+    
     def self.inherited subclass
       Extractula.add_extractor subclass
     end
@@ -40,6 +99,18 @@ module Extractula
           self.class.#{field}_attr
         end
       EOS
+    end
+    
+    def self.use_defaults *fields
+      fields.each do |field|
+        default = DEFAULTS[field]
+        case default
+        when String, Array
+          __send__ :"#{field}_path", *default
+        when Proc
+          define_method field, &default
+        end
+      end
     end
     
     attr_reader :url, :html, :content
